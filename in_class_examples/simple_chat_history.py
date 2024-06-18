54ab6ab8d4094c2cf7e6a33fa6e3305a14c3d162
Julian Oquendo <42851264+joqu86@users.noreply.github.com>
2024-06-18 13:15:38 -0400

    Update simple_chat_history.py
    removing unnecessary chatprompttemplates

--- in_class_examples/simple_chat_history.py
+++ in_class_examples/simple_chat_history.py
@@ -1,16 +1,5 @@
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Answer all questions to the best of your ability.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
 
 chat = ChatOpenAI()
 
